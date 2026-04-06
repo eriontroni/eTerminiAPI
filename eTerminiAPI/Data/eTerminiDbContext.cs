286fe9f3e3788b0bd48fcbd7ed082a03121611f5
eriontroni <142906809+eriontroni@users.noreply.github.com>
2026-04-06 16:41:55 +0200

    Add project files.

---
+++ eTerminiAPI/Data/eTerminiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eTerminiAPI.Data
+{
+    public class eTerminiDbContext : DbContext
+    {
+        public eTerminiDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        //qetu i shtojm DbSet-et per tabelat qe do te kemi ne database
+    }
+}
---
+++ eTerminiAPI/Program.cs
@@ -0,0 +1,32 @@
+using AutoMapper;
+using eTerminiAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(cfg => { }, AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddDbContext<eTerminiDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
